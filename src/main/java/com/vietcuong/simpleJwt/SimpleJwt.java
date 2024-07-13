9f2486ecd1e97ef73596b06a72e0b05debfa9558
vietcuongwork <vietcuongwork511@gmail.com>
2024-07-13 17:11:48 +0700

    minor change

--- src/main/java/com/vietcuong/simpleJwt/SimpleJwt.java
+++ src/main/java/com/vietcuong/simpleJwt/SimpleJwt.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleJwtAndValidationApplication {
+public class SimpleJwt {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleJwtAndValidationApplication.class, args);
+        SpringApplication.run(SimpleJwt.class, args);
     }
 
 }
